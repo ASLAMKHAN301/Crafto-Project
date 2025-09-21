41bbe29f4eb0dd8e6c0b950eb308f1a0fc97d9af
aslam1003 <aslamkhanp301@gmail.com>
2025-09-21 13:15:22 +0530

    Fix import case issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Navbar } from "./Components/Navbar";
 import { Home } from "./Components/Home";
-import { Contact } from "./Components/contact";
+import { Contact } from "./Components/Contact";
 import Footer from "./Components/Footer";
 
 
